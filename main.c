0341645767be3c3cd69e59bf00cf5306ee828127
Sebas Vagrant Lenovo <lopez.sebastian@correounivalle.edu.co>
2019-11-25 23:08:18 +0000

    Signal of ctrl + C added

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include "holberton.h"
 
-
+void controlC(int var)
+{
+	write(1, "\n($) ", 5);
+	(void) var;
+}
 /**
  * main - Simple Shell Project
  * Return: nothing
@@ -8,7 +12,7 @@
 int main(int argc, char **argv)
 {
 	(void) argc;
-
+	signal(SIGINT, controlC);
 	shellLoop(argv);
 
 	return (0);
